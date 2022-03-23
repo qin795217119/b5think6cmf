8ec0dd776d49a820080a6a4284933ad9cceaec5f
冰舞网络 <357145480@qq.com>
2022-03-23 12:33:43 +0800

    1

--- app/admin/controller/CommonController.php
+++ app/admin/controller/CommonController.php
@@ -9,8 +9,11 @@ declare (strict_types=1);
 namespace app\admin\controller;
 
 use app\admin\BaseController;
+use app\admin\extend\helpers\LoginAuthHelper;
 use app\common\helpers\Result;
 use app\common\helpers\UploadHelper;
+use app\common\model\Admin;
+use think\facade\Validate;
 use think\response\Json;
 
 
@@ -77,5 +80,35 @@ class CommonController extends BaseController
             'cat' => $this->request->request('cat',''),
         ];
         return $this->render('',$data);
+    }
+
+    //修改密码
+    public function repassAction(){
+        if($this->request->isPost()){
+            $rules = [
+                'oldpass|旧密码' => 'require',
+                'newpass|新密码' => 'require|min:6|max:20',
+                'confirmpass|确认密码' => 'require|min:6|max:20|confirm:newpass',
+            ];
+            $data = $this->request->post();
+            //验证数据格式
+            $validate = Validate::rule($rules);
+            if (!$validate->check($data)) {
+                return Result::error($validate->getError() ?: '表单数据错误');
+            }
+            if($data['oldpass'] == $data['newpass']){
+                return Result::error('新旧密码不能一样');
+            }
+            $adminInfo = Admin::bFind(LoginAuthHelper::adminLoginInfo('info.id'));
+            if(!$adminInfo) return Result::error('登录信息不存在');
+            if(md5($data['oldpass'])!=$adminInfo['password']){
+                return Result::error('旧密码不正确');
+            }
+            Admin::bUpdate(['id'=>$adminInfo['id'],'password'=>md5($data['newpass'])]);
+            return Result::error('密码修改成功');
+        }else{
+            return $this->render();
+        }
+
     }
 }
