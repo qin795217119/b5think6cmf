be41d26d79da79da7351ee0bcf99ca6530870657
冰舞网络 <357145480@qq.com>
2022-03-23 13:08:49 +0800

    1

--- app/admin/controller/CommonController.php
+++ app/admin/controller/CommonController.php
@@ -10,6 +10,7 @@ namespace app\admin\controller;
 
 use app\admin\BaseController;
 use app\admin\extend\helpers\LoginAuthHelper;
+use app\admin\extend\services\AdminLoginService;
 use app\common\helpers\Result;
 use app\common\helpers\UploadHelper;
 use app\common\model\Admin;
@@ -99,16 +100,40 @@ class CommonController extends BaseController
             if($data['oldpass'] == $data['newpass']){
                 return Result::error('新旧密码不能一样');
             }
-            $adminInfo = Admin::bFind(LoginAuthHelper::adminLoginInfo('info.id'));
-            if(!$adminInfo) return Result::error('登录信息不存在');
-            if(md5($data['oldpass'])!=$adminInfo['password']){
-                return Result::error('旧密码不正确');
+            $service = new AdminLoginService();
+            $service->getUser('id',LoginAuthHelper::adminLoginInfo('info.id'));
+            if(!$service->validatePassword($data['oldpass'])){
+                return  Result::error($service->message);
             }
-            Admin::bUpdate(['id'=>$adminInfo['id'],'password'=>md5($data['newpass'])]);
+            Admin::bUpdate(['id'=>$service->_user['id'],'password'=>md5($data['newpass'])]);
             return Result::success('密码修改成功');
         }else{
             return $this->render();
         }
+    }
 
+    public function lockscreenAction(){
+        if($this->request->isPost()){
+            $password = $this->request->post('password');
+            if(!$password) return Result::error('请输入密码');
+            $service = new AdminLoginService();
+            $adminId = LoginAuthHelper::adminLoginInfo('info.id');
+            if(!$adminId){
+                return Result::error('登录失效，请重新登录');
+            }
+            $service->getUser('id',$adminId);
+            if(!$service->validatePassword($password)){
+                return Result::error($service->message);
+            }
+            if($service->_user['status']!=1){
+                return Result::error('用户状态错误，请重新登录');
+            }
+            session('islock',null);
+            return Result::success('登录成功');
+        }else{
+            session('islock',1);
+            $adminInfo = LoginAuthHelper::adminLoginInfo('info');
+            return $this->render('',['user'=>$adminInfo]);
+        }
     }
 }
